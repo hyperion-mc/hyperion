fe00a2463b749adaa5906567c25b4cc762912649
TestingPlant <44930139+TestingPlant@users.noreply.github.com>
2025-01-01 06:59:40 +0000

    reimplement RuntimeLifetime

--- crates/hyperion/src/common/util/lifetime.rs
+++ crates/hyperion/src/common/util/lifetime.rs
@@ -1,4 +1,8 @@
-use std::mem::{ManuallyDrop, size_of};
+use std::{
+    marker::PhantomData,
+    mem::{ManuallyDrop, size_of},
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
 /// # Safety
 /// Same safety requirements as [`std::mem::transmute`]. In addition, both types must have the same
@@ -47,5 +51,118 @@ hyperion_packet_macros::for_each_static_play_c2s_packet! {
 hyperion_packet_macros::for_each_lifetime_play_c2s_packet! {
     unsafe impl Lifetime for PACKET<'_> {
         type WithLifetime<'a> = PACKET<'a>;
+    }
+}
+
+pub struct RuntimeLifetime<T> {
+    value: T,
+    references: *const AtomicUsize,
+}
+
+impl<T: Lifetime> RuntimeLifetime<T> {
+    #[must_use]
+    pub const fn get<'a>(&'a self) -> &'a T::WithLifetime<'a> {
+        // SAFETY: The program will abort if `self` is not dropped before
+        // [`LifetimeTracker::assert_no_references`] is called. `'a` will expire before `self` is
+        // dropped. Therefore, it is safe to change these references to `'a`, because if `'a`
+        // were to live after [`LifetimeTracker::assert_no_references`] is called, the program
+        // would abort before user code could use the invalid reference.
+        unsafe { &*(&raw const self.value).cast::<T::WithLifetime<'a>>() }
+    }
+}
+
+impl<T> Drop for RuntimeLifetime<T> {
+    fn drop(&mut self) {
+        // SAFETY: `self.references` is safe to dereference because the underlying [`LifetimeTracker`] would
+        // have already aborted if it were dropped before this
+        unsafe {
+            (*self.references).fetch_sub(1, Ordering::Relaxed);
+        }
+    }
+}
+
+mod sealed {
+    pub trait Sealed {}
+}
+
+pub trait LifetimeHandle<'a>: sealed::Sealed + Copy + Clone {
+    #[must_use]
+    fn runtime_lifetime<T>(&self, value: T) -> RuntimeLifetime<T>
+    where
+        T: 'a;
+}
+
+#[derive(Copy, Clone)]
+struct LifetimeHandleObject<'a> {
+    references: &'a AtomicUsize,
+}
+impl sealed::Sealed for LifetimeHandleObject<'_> {}
+impl<'a> LifetimeHandle<'a> for LifetimeHandleObject<'a> {
+    fn runtime_lifetime<T>(&self, value: T) -> RuntimeLifetime<T>
+    where
+        T: 'a,
+    {
+        self.references.fetch_add(1, Ordering::SeqCst);
+
+        RuntimeLifetime {
+            value,
+            references: std::ptr::from_ref(self.references),
+        }
+    }
+}
+
+pub struct LifetimeTracker {
+    references: Box<AtomicUsize>,
+}
+
+impl LifetimeTracker {
+    pub fn assert_no_references(&mut self) {
+        // TODO: determine better ordering to use
+        let references = self.references.load(Ordering::SeqCst);
+        if references != 0 {
+            tracing::error!("{references} values were held too long - aborting");
+            // abort is needed to avoid a panic handler allowing those values to continue being
+            // used
+            std::process::abort();
+        }
+    }
+
+    /// # Safety
+    /// Data which might be passed to the resulting [`LifetimeHandle::runtime_lifetime`] and outlives the `'handle`
+    /// lifetime must only be dropped after [`LifetimeTracker::assert_no_references`] is called on this tracker. The
+    /// only purpose of the `'handle` lifetime is to allow users to control which values are allowed to be passed to
+    /// [`LifetimeHandle::runtime_lifetime`] since values passed there must outlive `'handle`. The length of the
+    /// `'handle` lifetime itself does not matter, and `'handle` may expire before
+    /// [`LifetimeTracker::assert_no_references`] is called.
+    ///
+    /// It is very important to **specify the `'handle` lifetime parameter**. Allowing `'handle` to be
+    /// inferred is likely to violate the above safety requirements.
+    #[must_use]
+    unsafe fn handle<'handle>(&'handle self) -> impl LifetimeHandle<'handle> {
+        // Storing the lifetime generic in a trait ([`LifetimeHandle`]) instead of a struct is necessary to prohibit
+        // casts to a shorter lifetime. If the [`LifetimeHandle`]'s lifetime could be shortened, the user could safely
+        // pass values of any lifetime to [`LifetimeHandle::runtime_lifetime`], which would defeat the purpose of the
+        // `'handle` lifetime.
+        LifetimeHandleObject::<'handle> {
+            references: &self.references,
+        }
+    }
+}
+
+impl Default for LifetimeTracker {
+    fn default() -> Self {
+        Self {
+            references: Box::new(AtomicUsize::new(0)),
+        }
+    }
+
+}
+
+impl Drop for LifetimeTracker {
+    fn drop(&mut self) {
+        // Even if data associated with this tracker will live for `'static`, the [`Box`] storing
+        // the references will be dropped, so this ensures that there are no
+        // [`RuntimeLifetime`]s which might still have a pointer to the references.
+        self.assert_no_references();
     }
 }
