c46b6f688127c1c6325301faa5e8afd2068ae978
TestingPlant <44930139+TestingPlant@users.noreply.github.com>
2025-05-22 19:27:36 +0000

    fix: use entity index as minecraft id
    Entity indices are unique for all living entities. The generation does
    not need to be included in the id.

--- crates/hyperion-utils/src/lib.rs
+++ crates/hyperion-utils/src/lib.rs
@@ -8,38 +8,23 @@ pub use lifetime::*;
 
 pub trait EntityExt: Sized {
     fn minecraft_id(&self) -> anyhow::Result<i32>;
-    fn from_minecraft_id(id: i32) -> anyhow::Result<Self>;
+    fn from_minecraft_id(id: i32, world: &World) -> anyhow::Result<Self>;
 }
 
-// todo(bevy): this is probably different with bevy
-
-// we need a mapping from Bevy entity id to Minecraft entity id
-// some
 impl EntityExt for Entity {
     fn minecraft_id(&self) -> anyhow::Result<i32> {
         let index = self.index();
-        let generation = self.generation();
-
-        let index_u16 = u16::try_from(index).unwrap();
-
-        let generation_u16 = u16::try_from(generation).unwrap();
-
-        let index_u32 = u32::from(index_u16);
-        let generation_u32 = u32::from(generation_u16);
-
-        let raw_u32 = index_u32 << 16 | generation_u32;
-
-        Ok(bytemuck::cast(raw_u32))
+        Ok(bytemuck::cast(index))
     }
 
-    fn from_minecraft_id(id: i32) -> anyhow::Result<Self> {
+    fn from_minecraft_id(id: i32, world: &World) -> anyhow::Result<Self> {
         let id: u32 = bytemuck::cast(id);
 
-        let index = id >> 16;
-        let generation = id & 0xFFFF;
-
-        let raw_u64 = u64::from(index) | (u64::from(generation) << 32);
-        Ok(Self::from_bits(raw_u64))
+        // TODO: According to the docs, this should check if the returned entity is freed
+        world
+            .entities()
+            .resolve_from_id(id)
+            .ok_or_else(|| anyhow::anyhow!("minecraft id is invalid"))
     }
 }
 
