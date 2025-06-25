07c1cdf48d067241f396b2bd62e2b630ed33363e
TestingPlant <44930139+TestingPlant@users.noreply.github.com>
2025-06-25 02:10:27 +0000

    fix(docs): fix doc links

--- crates/hyperion/src/simulation/packet_state.rs
+++ crates/hyperion/src/simulation/packet_state.rs
@@ -1,8 +1,8 @@
 //! Components marking a player packet state. Players will have at most 1 state component at a time (they may have no components during state transitions)
 //!
 //! All players with a state component assigned are guaranteed to have the following components:
-/// - [`hyperion::ConnectionId`]
-/// - [`hyperion::PacketDecoder`]
+/// - [`crate::ConnectionId`]
+/// - [`crate::PacketDecoder`]
 use bevy::prelude::*;
 
 /// Marks players who are in the handshake state.
@@ -20,18 +20,18 @@ pub struct Login(pub(crate) ());
 /// Marks players who are in the play state.
 ///
 /// Players in this state are guaranteed to have the following components:
-/// - [`hyperion::simulation::Name`]
-/// - [`hyperion::simulation::Uuid`]
-/// - [`hyperion::simulation::AiTargetable`]
-/// - [`hyperion::simulation::ImmuneStatus`]
-/// - [`hyperion::simulation::ChunkPosition`]
-/// - [`hyperion::simulation::ChunkSendQueue`]
-/// - [`hyperion::simulation::Yaw`]
-/// - [`hyperion::simulation::Pitch`]
+/// - [`crate::simulation::Name`]
+/// - [`crate::simulation::Uuid`]
+/// - [`crate::simulation::AiTargetable`]
+/// - [`crate::simulation::ImmuneStatus`]
+/// - [`crate::simulation::ChunkPosition`]
+/// - [`crate::egress::sync_chunks::ChunkSendQueue`]
+/// - [`crate::simulation::Yaw`]
+/// - [`crate::simulation::Pitch`]
 ///
 /// They may, but are not required to, have the following components:
-/// - [`hyperion::simulation::skin::PlayerSkin`]: is added once the player skin loads
-/// - [`hyperion::simulation::Position`]: is not set by Hyperion - the event code must add this
+/// - [`crate::simulation::skin::PlayerSkin`]: is added once the player skin loads
+/// - [`crate::simulation::Position`]: is not set by Hyperion - the event code must add this
 ///   component
 #[derive(Component)]
 pub struct Play(pub(crate) ());
