c24383a58acd146d3408b3c83f4ee4d00e1096cd
TestingPlant <44930139+TestingPlant@users.noreply.github.com>
2025-06-26 15:40:18 +0000

    perf(metadata): use pre-encoded packet

--- crates/hyperion/src/egress/metadata.rs
+++ crates/hyperion/src/egress/metadata.rs
@@ -1,31 +1,19 @@
 //! Utilities for working with the Entity Metadata packet.
 
-use valence_bytes::Bytes;
-use valence_protocol::{Encode, RawBytes, VarInt, packets::play};
+use valence_protocol::{RawBytes, VarInt, packets::play};
 
 /// Packet to show all parts of the skin.
 #[must_use]
 pub fn show_all(id: i32) -> play::EntityTrackerUpdateS2c<'static> {
-    let entity_id = VarInt(id);
-
     // https://wiki.vg/Entity_metadata#Entity_Metadata_Format
     // https://wiki.vg/Entity_metadata#Player
     // 17 = Metadata, type = byte
-    let mut bytes = Vec::new();
-    bytes.push(17_u8);
+    static BYTES: &[u8] = &[17, 0, 0xff, 0xff];
 
-    #[expect(clippy::unwrap_used, reason = "this should never fail")]
-    VarInt(0).encode(&mut bytes).unwrap();
-
-    // all 1s
-    #[expect(clippy::unwrap_used, reason = "this should never fail")]
-    u8::MAX.encode(&mut bytes).unwrap();
-
-    // end with 0xff
-    bytes.push(0xff);
+    let entity_id = VarInt(id);
 
     play::EntityTrackerUpdateS2c {
         entity_id,
-        tracked_values: RawBytes(Bytes::from(bytes).into()),
+        tracked_values: RawBytes(BYTES.into()),
     }
 }
