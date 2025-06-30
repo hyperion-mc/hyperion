be206e7eaae3b09616fb708595385850c32d5c80
TestingPlant <44930139+TestingPlant@users.noreply.github.com>
2025-06-30 21:46:58 +0000

    feat: implement SetSkin handler

--- events/tag/src/skin.rs
+++ events/tag/src/skin.rs
@@ -1,117 +1,101 @@
 use std::borrow::Cow;
 
-use flecs_ecs::{
-    core::{
-        Entity, EntityView, EntityViewGet, QueryBuilderImpl, SystemAPI, TermBuilderImpl, World,
-    },
-    macros::{Component, system},
-    prelude::Module,
-};
+use bevy::prelude::*;
 use hyperion::{
     egress::player_join::{PlayerListActions, PlayerListEntry, PlayerListS2c},
     net::{Compose, ConnectionId, DataBundle},
-    simulation::{event, skin::PlayerSkin},
-    storage::EventQueue,
-    uuid::Uuid,
+    simulation::event,
     valence_ident::ident,
-    valence_protocol,
-    valence_protocol::{
-        GameMode, VarInt,
-        game_mode::OptGameMode,
-        packets::play::{EntitiesDestroyS2c, PlayerRemoveS2c, PlayerRespawnS2c},
-    },
 };
 use hyperion_utils::EntityExt;
-use tracing::debug;
+use tracing::error;
+use valence_bytes::Utf8Bytes;
+use valence_protocol::{
+    GameMode, VarInt,
+    game_mode::OptGameMode,
+    packets::play::{EntitiesDestroyS2c, PlayerRemoveS2c, PlayerRespawnS2c},
+};
 
-#[derive(Component)]
-pub struct SkinModule;
+pub struct SkinPlugin;
 
-impl Module for SkinModule {
-    fn module(world: &World) {
-        system!("set_skin", world, &mut EventQueue<event::SetSkin>($), &Compose($)).each_iter(
-            |it, _, (event_queue, compose)| {
-                let world = it.world();
-                let system = it.system();
-                for event in event_queue.drain() {
-                    debug!("got {event:?}");
-                    event
-                        .by
-                        .entity_view(world)
-                        .get::<(&ConnectionId, &hyperion::simulation::Uuid)>(|(io, uuid)| {
-                            on_set_skin(event.by, compose, system, uuid.0, event.skin, *io);
-                        });
-                }
-            },
-        );
+impl Plugin for SkinPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(FixedUpdate, on_set_skin);
     }
 }
 
 fn on_set_skin(
-    id: Entity,
-    compose: &Compose,
-    system: EntityView<'_>,
-    uuid: Uuid,
-    skin: PlayerSkin,
-    io: ConnectionId,
+    mut events: EventReader<'_, '_, event::SetSkin>,
+    compose: Res<'_, Compose>,
+    query: Query<'_, '_, (&ConnectionId, &hyperion::simulation::Uuid)>,
 ) {
-    let minecraft_id = id.minecraft_id();
-    let mut bundle = DataBundle::new(compose, system);
-    // Remove player info
-    bundle
-        .add_packet(&PlayerRemoveS2c {
-            uuids: Cow::Borrowed(&[uuid]),
-        })
-        .unwrap();
+    for event in events.read() {
+        let (&connection_id, uuid) = match query.get(event.by) {
+            Ok(data) => data,
+            Err(e) => {
+                error!("failed to set skin: query failed: {e}");
+                continue;
+            }
+        };
 
-    // Destroy player entity
-    bundle
-        .add_packet(&EntitiesDestroyS2c {
-            entity_ids: Cow::Borrowed(&[VarInt(minecraft_id)]),
-        })
-        .unwrap();
+        let minecraft_id = event.by.minecraft_id();
+        let mut bundle = DataBundle::new(&compose);
+        // Remove player info
+        bundle
+            .add_packet(&PlayerRemoveS2c {
+                uuids: Cow::Borrowed(&[**uuid]),
+            })
+            .unwrap();
 
-    // todo: in future, do not clone
-    let property = valence_protocol::profile::Property {
-        name: "textures".to_string(),
-        value: skin.textures,
-        signature: Some(skin.signature),
-    };
+        // Destroy player entity
+        bundle
+            .add_packet(&EntitiesDestroyS2c {
+                entity_ids: Cow::Borrowed(&[VarInt(minecraft_id)]),
+            })
+            .unwrap();
 
-    let property = &[property];
+        // todo: in future, do not clone
+        let property = valence_protocol::profile::Property::<Utf8Bytes> {
+            name: "textures".into(),
+            value: event.skin.textures.clone().into(),
+            signature: Some(event.skin.signature.clone().into()),
+        };
 
-    // Add player back with new skin
-    bundle
-        .add_packet(&PlayerListS2c {
-            actions: PlayerListActions::default().with_add_player(true),
-            entries: Cow::Borrowed(&[PlayerListEntry {
-                player_uuid: uuid,
-                username: Cow::Borrowed("Player"),
-                properties: Cow::Borrowed(property),
-                chat_data: None,
-                listed: true,
-                ping: 20,
+        let property = &[property];
+
+        // Add player back with new skin
+        bundle
+            .add_packet(&PlayerListS2c {
+                actions: PlayerListActions::default().with_add_player(true),
+                entries: Cow::Borrowed(&[PlayerListEntry {
+                    player_uuid: **uuid,
+                    username: "Player".into(),
+                    properties: Cow::Borrowed(property),
+                    chat_data: None,
+                    listed: true,
+                    ping: 20,
+                    game_mode: GameMode::Survival,
+                    display_name: None,
+                }]),
+            })
+            .unwrap();
+
+        // // Respawn player
+        bundle
+            .add_packet(&PlayerRespawnS2c {
+                dimension_type_name: ident!("minecraft:overworld"),
+                dimension_name: ident!("minecraft:overworld"),
+                hashed_seed: 0,
                 game_mode: GameMode::Survival,
-                display_name: None,
-            }]),
-        })
-        .unwrap();
+                previous_game_mode: OptGameMode::default(),
+                is_debug: false,
+                is_flat: false,
+                copy_metadata: false,
+                last_death_location: None,
+                portal_cooldown: VarInt::default(),
+            })
+            .unwrap();
 
-    // // Respawn player
-    bundle
-        .add_packet(&PlayerRespawnS2c {
-            dimension_type_name: ident!("minecraft:overworld").into(),
-            dimension_name: ident!("minecraft:overworld").into(),
-            hashed_seed: 0,
-            game_mode: GameMode::Survival,
-            previous_game_mode: OptGameMode::default(),
-            is_debug: false,
-            is_flat: false,
-            copy_metadata: false,
-            last_death_location: None,
-            portal_cooldown: VarInt::default(),
-        })
-        .unwrap();
-
-    bundle.unicast(io).unwrap();
+        bundle.unicast(connection_id).unwrap();
+    }
 }
