0b730f55255ac115b449acee0adc021343201931
TestingPlant <44930139+TestingPlant@users.noreply.github.com>
2025-06-30 21:47:28 +0000

    feat(tag): add plugins

--- events/tag/src/lib.rs
+++ events/tag/src/lib.rs
@@ -7,107 +7,50 @@ use std::{collections::HashSet, net::SocketAddr};
 
 use bevy::prelude::*;
 use hyperion::{
-    HyperionCore,
-    SetEndpoint,
+    HyperionCore, SetEndpoint,
     simulation::{EntitySize, Position, packet_state},
-    // simulation::{Player, Position},
     spatial::{Spatial, SpatialIndex},
 };
 use hyperion_proxy_module::SetProxyAddress;
 use tracing::error;
 
-// use hyperion_clap::hyperion_command::CommandRegistry;
-// use hyperion_gui::Gui;
-// use hyperion_rank_tree::Team;
-// use module::{
-//     attack::AttackModule, block::BlockModule, damage::DamageModule, level::LevelModule,
-//     regeneration::RegenerationModule, vanish::VanishModule,
-// };
-// use spatial::SpatialIndex;
-use crate::module::{
-    chat::ChatPlugin, spawn::SpawnPlugin, stats::StatsPlugin, vanish::VanishPlugin,
+use crate::{
+    module::{
+        attack::AttackPlugin, block::BlockPlugin, bow::BowPlugin, chat::ChatPlugin,
+        damage::DamagePlugin, level::LevelPlugin, regeneration::RegenerationPlugin,
+        spawn::SpawnPlugin, stats::StatsPlugin, vanish::VanishPlugin,
+    },
+    skin::SkinPlugin,
 };
-// use crate::{
-//     module::{bow::BowModule, chat::ChatModule, spawn::SpawnModule, stats::StatsModule},
-//     skin::SkinModule,
-// };
 
 mod command;
 mod module;
-// mod skin;
+mod skin;
 
 #[derive(Resource, Default, Deref, DerefMut)]
 struct OreVeins {
     ores: HashSet<IVec3>,
 }
-// #[derive(Component, Deref, DerefMut)]
-// struct MainBlockCount(i8);
-//
-// impl Default for MainBlockCount {
-//     fn default() -> Self {
-//         Self(16)
-//     }
-// }
-//
+
+#[derive(Component, Deref, DerefMut)]
+struct MainBlockCount(i8);
+
+impl Default for MainBlockCount {
+    fn default() -> Self {
+        Self(16)
+    }
+}
+
 #[derive(Component)]
 struct FollowClosestPlayer;
-// impl Module for TagModule {
-//     fn module(world: &World) {
-//         // on entity kind set UUID
-//
-//         world.component::<FollowClosestPlayer>();
-//         world.component::<MainBlockCount>();
-//         world.component::<Gui>();
-//
-//         world
-//             .component::<Player>()
-//             .add_trait::<(flecs::With, MainBlockCount)>();
-//
-//         world.import::<hyperion_rank_tree::RankTree>();
-//
-//         world
-//             .component::<Player>()
-//             .add_trait::<(flecs::With, Team)>();
-//
-//         world.import::<SpawnModule>();
-//         world.import::<ChatModule>();
-//         world.import::<StatsModule>();
-//         world.import::<BlockModule>();
-//         world.import::<hyperion_respawn::RespawnModule>();
-//         world.import::<AttackModule>();
-//         world.import::<LevelModule>();
-//         world.import::<BowModule>();
-//         world.import::<RegenerationModule>();
-//         world.import::<hyperion_permission::PermissionModule>();
-//         world.import::<hyperion_utils::HyperionUtilsModule>();
-//         world.import::<hyperion_clap::ClapCommandModule>();
-//         world.import::<SkinModule>();
-//         world.import::<VanishModule>();
-//         world.import::<DamageModule>();
-//
-//         world.get::<&mut CommandRegistry>(|registry| {
-//             command::register(registry, world);
-//         });
-//
-//         world.set(hyperion_utils::AppId {
-//             qualifier: "com".to_string(),
-//             organization: "andrewgazelka".to_string(),
-//             application: "hyperion-poc".to_string(),
-//         });
-//
-//         // import spatial module and index all players
-//         world.import::<spatial::SpatialModule>();
-//         world
-//             .component::<Player>()
-//             .add_trait::<(flecs::With, spatial::Spatial)>();
-//     }
-// }
 
 fn initialize_player(
     trigger: Trigger<'_, OnAdd, packet_state::Play>,
     mut commands: Commands<'_, '_>,
 ) {
-    commands.entity(trigger.target()).insert(Spatial);
+    commands
+        .entity(trigger.target())
+        .insert((Spatial, MainBlockCount::default()));
 }
 
 fn follow_closest_player(
@@ -170,10 +113,19 @@ impl Plugin for TagPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(OreVeins::default());
         app.add_plugins((
-            ChatPlugin,
-            StatsPlugin,
-            SpawnPlugin,
-            VanishPlugin,
+            (
+                AttackPlugin,
+                BlockPlugin,
+                BowPlugin,
+                ChatPlugin,
+                DamagePlugin,
+                LevelPlugin,
+                RegenerationPlugin,
+                SkinPlugin,
+                SpawnPlugin,
+                StatsPlugin,
+                VanishPlugin,
+            ),
             hyperion_clap::ClapCommandPlugin,
             hyperion_genmap::GenMapPlugin,
             hyperion_item::ItemPlugin,
